e143833ac25a6e16107a4f127cba225378c03877
AdrianKrampikowski <akrampi.handy@gmail.com>
2023-03-11 12:39:37 +0100

    first Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Quelle: https://developer-akademie.teachable.com/courses/902235/lectures/31232815
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
