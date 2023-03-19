ccf674a87e569edfaea800aa92adf0d2d2ee7557
Fausto Oliveira <105312418+FaustoOliveira@users.noreply.github.com>
2023-03-19 16:17:14 +0100

    next commit

---
+++ addTask.js
@@ -0,0 +1,17 @@
+function openCategory() {
+
+    if (newCategorySelected) {
+        return; // Function cancel
+    }
+
+    // Öffnen - Wird nicht ausgeführt nach einem return
+    let category = document.getElementById('addTask_categoryList');
+    if (category.classList.contains('d-none')) {
+        category.classList.remove('d-none');
+        document.getElementById('addTask_borderButton').classList.add('borderButton');
+    } else {
+        category.classList.add('d-none');
+        document.getElementById('addTask_borderButton').classList.remove('borderButton');
+    }
+
+}
