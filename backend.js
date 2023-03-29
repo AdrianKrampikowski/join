5849e53fba442f66cf7c1c4993786e01754304a0
AdrianKrampikowski <akrampi.handy@gmail.com>
2023-03-29 22:24:55 +0200

    next commit

--- backend.js
+++ backend.js
@@ -11,16 +11,16 @@ async function init() {
     users = JSON.parse(backend.getItem('users')) || [];
     contacts = JSON.parse(backend.getItem('contacts')) || [];
     tasks = JSON.parse(backend.getItem('tasks')) || [];
-} 
+}
 
 
 // ================================================ REGISTRIEREN ==========================================================
 // Am besten eine separate "register.js" Datei erstellen
-function generateUserId(){
+function generateUserId() {
     id = Math.floor((Math.random() * 1000000) + 1);
 }
 
-function addUser(){
+function addUser() {
     generateUserId();
     let name = document.getElementById('name');
     let surname = document.getElementById('surname');
@@ -30,20 +30,20 @@ function addUser(){
     let userId = id;
 
     for (let i = 0; i < users.length; i++) {
-        if(users[i]['userid'].includes === id) {
+        if (users[i]['userid'].includes === id) {
             generateUserId();
-        } 
+        }
     }
 
-    let userData = {name: name.value, surname: surname.value, email: email.value, password: password.value, color: color.value, userid: userId};
+    let userData = { name: name.value, surname: surname.value, email: email.value, password: password.value, color: color.value, userid: userId };
     let user = users.find(u => u.email == email.value && u.password == password.value);
-    
-    if(user){
+
+    if (user) {
         alreadySignedUpPupup();
         name.value = '';
         surname.value = '';
         email.value = '';
-        password.value ='';
+        password.value = '';
     } else {
         users.push(userData);
         save();
@@ -52,20 +52,20 @@ function addUser(){
         setInterval(forwardToLogin, 1200);
     }
 
-//    let urlParams = new URLSearchParams(window.location.search);     // query paramter von einem query string (window.location.search) von der URL auslesen
-//    let idMsg = urlParams.get('id');
+    //    let urlParams = new URLSearchParams(window.location.search);     // query paramter von einem query string (window.location.search) von der URL auslesen
+    //    let idMsg = urlParams.get('id');
 }
 
 // ToDoStart
-function createTask(){
+function createTask() {
     // generateUserId();
     let title = document.getElementById('title');
     let description = document.getElementById('description');
     let category = document.getElementById('categoryChoices');
-    let assignTo = document.getElementById('assignedToUsers');
+    let assignTo = selectedValues;
     let dueDate = document.getElementById('dueDate');
     // let priority = selectedPriority;
-    let taskData = {title: title.value, description: description.value, category: category.value, assignTo: assignTo.value, dueDate: dueDate.value};
+    let taskData = { title: title.value, description: description.value, category: category.value, assignTo: assignTo, dueDate: dueDate.value };
     // tasks.push(taskData);
     // saveTasks();
     console.log("taskdata", taskData);
@@ -84,7 +84,7 @@ function createTask(){
 
     // let userData = {name: name.value, surname: surname.value, email: email.value, password: password.value, color: color.value, userid: userId};
     // let user = users.find(u => u.email == email.value && u.password == password.value);
-    
+
     // if(user){
     //     alreadySignedUpPupup();
     //     name.value = '';
@@ -98,11 +98,38 @@ function createTask(){
 
     //     setInterval(forwardToLogin, 1200);
     // }
-} 
+}
 async function saveTasks() {
     let tasksAsString = JSON.stringify(tasks);
     await backend.setItem('tasks', tasksAsString);
 }
+
+// Define an empty array to store the selected values
+selectedValues = [];
+setTimeout(() => {
+    saveSelectedUsers();
+}, 1500);
+
+// Add an event listener to the checkboxes to update the selectedValues array
+function saveSelectedUsers() {
+    document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+        checkbox.addEventListener('change', (event) => {
+            const selectedValue = event.target.value;
+            if (event.target.checked) {
+                if (!selectedValues.includes(selectedValue)) { // Check for duplicates
+                    selectedValues.push(selectedValue);
+                }
+            } else {
+                const index = selectedValues.indexOf(selectedValue);
+                if (index > -1) {
+                    selectedValues.splice(index, 1);
+                }
+            }
+            console.log(selectedValues); // Print the selected values to the console
+        });
+    });
+}
+
 //ToDoEnd
 
 function forwardToLogin() {
@@ -112,8 +139,8 @@ function forwardToLogin() {
 }
 
 async function save() {
-       let usersAsString = JSON.stringify(users);
-       await backend.setItem('users', usersAsString);
+    let usersAsString = JSON.stringify(users);
+    await backend.setItem('users', usersAsString);
 }
 
 function backToLoginScreen() {
@@ -124,11 +151,11 @@ function backToLoginScreen() {
 function login() {
     let emailLog = document.getElementById('emailLog');
     let passwordLog = document.getElementById('passwordLog');
-    
+
     let user = users.find(u => u.email == emailLog.value && u.password == passwordLog.value);
     let existingUser = users.find(u => u.email == emailLog.value);
-    
-    if(user) {
+
+    if (user) {
         let urlParams = new URLSearchParams(window.location.search); // query paramter von einem query string (window.location.search) von der URL auslesen
         let userId = urlParams.get('id');
         let userName = user.name;
@@ -141,10 +168,10 @@ function login() {
     }
 }
 
-function guestLogin(){
+function guestLogin() {
     let userName = "Guest";
     localStorage.setItem('userName', userName);
-    window.location.href = 'summery.html';  
+    window.location.href = 'summery.html';
 }
 
 
@@ -181,43 +208,43 @@ function guestLogin(){
 function alreadySignedUpPupup() {
     // Get the snackbar DIV
     var x = document.getElementById("alreadySignedUp");
-  
+
     // Add the "show" class to DIV
     x.className = "show";
-  
+
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 
 function successfullySignedUpPopup() {
     // Get the snackbar DIV
     var x = document.getElementById("successfullySignedUp");
-  
+
     // Add the "show" class to DIV
     x.className = "show";
-  
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-  }
 
-  function userDoesNotExistPopup() {
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function userDoesNotExistPopup() {
     // Get the snackbar DIV
     var x = document.getElementById("userDoesNotExist");
-  
+
     // Add the "show" class to DIV
     x.className = "show";
-  
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-  }
 
-  function pwEmailIncorrectPopup() {
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function pwEmailIncorrectPopup() {
     // Get the snackbar DIV
     var x = document.getElementById("pwEmailIncorrect");
-  
+
     // Add the "show" class to DIV
     x.className = "show";
-  
+
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-  }
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
--- js/createTask.js
+++ js/createTask.js
@@ -1,11 +1,38 @@
 
-function createDropdownUsers(){
+function createDropdownUsers() {
     for (let i = 0; i < users.length; i++) {
         document.getElementById("assignedToUsers").innerHTML += `
-        <option value="${users[i]["id"]}">${users[i]["name"]}</option>
-        `; 
+        <option value="${users[i]["id"]}"><input type="checkbox">${users[i]["name"]}</option>
+        `;
     }
 }
 setTimeout(() => {
-    createDropdownUsers();
+    // createDropdownUsers()
+    // createDropdownCheckboxes();
+    // getSelectedUsers();
+    addAssignedToList();
+
 }, 1250);
+
+function addAssignedToList() {
+    document.getElementById('assignedToChoices').innerHTML = '';
+    for (let i = 0; i < users.length; i++) {
+        let userID = users[i]["id"];
+        // let contact = users[i];
+        let name = users[i]["name"];
+        document.getElementById('assignedToChoices').innerHTML += `
+        <div class="assigned-to-line"><label for="assigned-to-${i}" 
+        id="assigned_name${i}">${name}
+        </label><input type="checkbox" id="assigned-to-${i}" 
+        value="${name}"></div>`
+    }
+}
+
+function openDropdown(id) {
+    if (document.getElementById(id).classList.contains('d-none')) {
+        document.getElementById(id).classList.remove('d-none');
+    }
+    else if (!document.getElementById(id).classList.contains('d-none')) {
+        document.getElementById(id).classList.add('d-none');
+    }
+}
